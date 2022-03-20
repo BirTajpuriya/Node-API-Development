7708da0ed277e947eb89c7d2f5ac6ac3da299d76
BirTajpuriya <tajpuriyabir00@gmail.com>
2022-03-20 23:17:02 +0545

    routing and controllers

---
+++ DEVCAMPER_API/controllers/bootcamps.js
@@ -0,0 +1,14 @@
+
+exports.getBootcamps=(req,res,next)=>{
+        res.status(200).json({success:true, msg:'show bootcamps'});
+}
+
+exports.CreateBootcamp=(req,res,next)=>{
+        res.status(200).json({success:true, msg:'create bootcamps'});
+}
+exports.UpdateBootcamp=(req,res,next)=>{
+        res.status(200).json({success:true, msg:`update bootcamp ${req.params.id}`});
+}
+exports.DeleteBootcamp=(req,res,next)=>{
+        res.status(200).json({success:true, msg:`Delete bootcamps ${req.params.id}`});
+}
